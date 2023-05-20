ca2b80b175432659e4e6a5f34ac7b390af24adfd
loubna saadi <loubnasaadi88@gmail.com>
2023-05-20 22:10:13 +0100

    1111

--- lib/screens/Profile_screen.dart
+++ lib/screens/Profile_screen.dart
@@ -22,24 +22,23 @@ class _ProfileScreenState extends State<ProfileScreen> {
     getCurrentUser();
   }
 
-void getCurrentUser() async {
-  final user = _auth.currentUser;
-  if (user != null) {
-    setState(() {
-      currentUser = user;
-    });
-
-    final userRef = FirebaseFirestore.instance.collection('test').doc(user.uid);
-    final snapshot = await userRef.get();
-    if (snapshot.exists) {
-      print('Snapshot data: ${snapshot.data()}');
+  void getCurrentUser() async {
+    final user = _auth.currentUser;
+    if (user != null) {
       setState(() {
-        userData = snapshot.data() ?? {};
+        currentUser = user;
       });
+
+      final userRef = FirebaseFirestore.instance.collection('test').doc(user.uid);
+      final snapshot = await userRef.get();
+      if (snapshot.exists) {
+        print('Snapshot data: ${snapshot.data()}');
+        setState(() {
+          userData = snapshot.data() ?? {};
+        });
+      }
     }
   }
-}
-
 
   @override
   Widget build(BuildContext context) {
@@ -52,10 +51,16 @@ void getCurrentUser() async {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
+                  userData['profileImage'] != null
+                      ? CircleAvatar(
+                          backgroundImage: NetworkImage(userData['profileImage']),
+                          radius: 50,
+                        )
+                      : Container(), // Display an empty container if profileImage is not available
                   Text('Name: ${userData['name']}'),
                   Text('Email: ${userData['email']}'),
                   Text('City: ${userData['city']}'),
-                  Text('Specialty: ${userData['speciality']}'),
+                  Text('Specialty: ${userData['specialty']}'),
                 ],
               ),
             )
@@ -69,126 +74,4 @@ void getCurrentUser() async {
 
 
 
-
-
-
-
-
-
-
-
-// import 'package:flutter/material.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:cloud_firestore/cloud_firestore.dart';
-
-// class ProfileScreen extends StatefulWidget {
-//   static const String screenRoute = 'profile_screen';
-
-//   const ProfileScreen({Key? key}) : super(key: key);
-
-//   @override
-//   _ProfileScreenState createState() => _ProfileScreenState();
-// }
-
-// class _ProfileScreenState extends State<ProfileScreen> {
-//   final _auth = FirebaseAuth.instance;
-//   final _nameController = TextEditingController();
-//   final _emailController = TextEditingController();
-//   final _cityController = TextEditingController();
-//   final _specialityController = TextEditingController();
-
-//   late User? currentUser;
-//   late Map<String, dynamic> userData;
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     getCurrentUser();
-//   }
-
-//   void getCurrentUser() async {
-//     final user = _auth.currentUser;
-//     if (user != null) {
-//       setState(() {
-//         currentUser = user;
-//       });
-
-//       final userRef = FirebaseFirestore.instance.collection('test').doc(user.uid);
-//       final DocumentSnapshot<Map<String, dynamic>> snapshot = await userRef.get();
-//       userData = snapshot.data()!;
-
-//       setState(() {
-//         _nameController.text = userData['name'];
-//         _emailController.text = userData['email'];
-//         _cityController.text = userData['city'];
-//         _specialityController.text = userData['speciality'];
-//       });
-//     }
-//   }
-
-//   Future<void> _updateProfile() async {
-//   try {
-//     final User? user = _auth.currentUser;
-//     if (user != null) {
-//       final userRef =
-//           FirebaseFirestore.instance.collection('test').doc(user.uid);
-//       await userRef.set({
-//         'name': _nameController.text,
-//         'email': _emailController.text,
-//         'city': _cityController.text,
-//         'speciality': _specialityController.text,
-//       }, SetOptions(merge: true));
-
-//       setState(() {
-//         userData = userRef.get() as Map<String, dynamic>;
-//       });
-
-//       ScaffoldMessenger.of(context).showSnackBar(
-//         SnackBar(content: Text('Profile updated successfully')),
-//       );
-//     }
-//   } catch (e) {
-//     ScaffoldMessenger.of(context).showSnackBar(
-//       SnackBar(content: Text('Error updating profile')),
-//     );
-//   }
-// }
-
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Profile'),
-//       ),
-//       body: Padding(
-//         padding: const EdgeInsets.all(16.0),
-//         child: Column(
-//           children: [
-//             TextField(
-//               controller: _nameController,
-//               decoration: InputDecoration(labelText: 'Name'),
-//             ),
-//             TextField(
-//               controller: _emailController,
-//               decoration: InputDecoration(labelText: 'Email'),
-//             ),
-//             TextField(
-//               controller: _cityController,
-//               decoration: InputDecoration(labelText: 'City'),
-//             ),
-//             TextField(
-//               controller: _specialityController,
-//               decoration: InputDecoration(labelText: 'Speciality'),
-//             ),
-//             ElevatedButton(
-//               onPressed: _updateProfile,
-//               child: Text('Update Profile'),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
 
